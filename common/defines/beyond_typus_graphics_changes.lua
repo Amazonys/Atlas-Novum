c90e17f4b19abfa8ee5ace1ab02f4c7f6c77da52
Amazonys <orionamyzonis@gmail.com>
2018-10-19 12:38:07 -0500

    10/19 Secret Development build

---
+++ common/defines/beyond_typus_graphics_changes.lua
@@ -0,0 +1,10 @@
+ --NGraphics changes
+NDefines.NGraphics.SHIP_IN_PORT_SCALE = 0.5			-- from 0p25
+NDefines.NGraphics.BORDER_WIDTH = 0.8			-- from 1p5
+NDefines.NGraphics.CAPITAL_INDICATOR_HEIGHT = 3.3			-- from 5p8
+NDefines.NGraphics.CAPITAL_INDICATOR_HEIGHT_SCALE = 170.0			-- from 140p0
+
+--NFrontend Changes
+NDefines.NFrontend.CAMERA_MIN_DIST_FOR_ROTATE = 400.0			-- from 800p0
+NDefines.NFrontend.CAMERA_MIN_HEIGHT = 40.0			-- from 50p0
+NDefines.NFrontend.CAMERA_MAX_HEIGHT = 3750.0			-- from 3000p0
