a04e689fee6887cdc5bbd533ce9aac628d8c2ca9
Amazonys <orionamyzonis@gmail.com>
2023-03-20 21:44:05 -0500

    Building Back Better

--- AN_Core/common/defines/AN_BT_defines.lua
+++ AN_Core/common/defines/AN_BT_defines.lua
@@ -154,15 +154,15 @@ NDefines.NCountry.REVOLT_TECH_MORALE = 0.01
 NDefines.NCountry.CROWN_LANDS_ALERT_THRESHOLD = 35
 NDefines.NCountry.MIN_POPULATION_FOR_TRADEGOOD_ASSIGNMENT = 1001
 
---NDefines.NCountry.CULTURE_LOSS_THRESHOLD = 0.10 					-- _CDEF_CULTURE_LOSS_THRESHOLD = 10,
---NDefines.NCountry.CULTURE_GAIN_THRESHOLD = 0.20 					-- _CDEF_CULTURE_GAIN_THRESHOLD = 10,
+NDefines.NCountry.CULTURE_LOSS_THRESHOLD = 0.05 					-- _CDEF_CULTURE_LOSS_THRESHOLD = 10,
+NDefines.NCountry.CULTURE_GAIN_THRESHOLD = 0.10 					-- _CDEF_CULTURE_GAIN_THRESHOLD = 10,
 NDefines.NCountry.CULTURE_COST_DIFF_ORIGINAL = -10			-- NOW MINORITY NOT ORIGINAL	-- Difference in cost in percent to convert to original culture.
 NDefines.NCountry.CULTURE_COST_DIFF_ADJACENT = -5				-- Difference in cost in percent to convert to adjacent culture.
 NDefines.NCountry.CULTURE_COST_DIFF_PRIMARY = 0				-- Difference in cost in percent to convert to primary culture.
-NDefines.NCountry.CULTURE_MIN_DEVELOPMENT_TO_PROMOTE = 99999		-- Minimum development required to promote the culture.
-NDefines.NCountry.PS_SET_PRIMARY_CULTURE = 200
-NDefines.NCountry.PS_ADD_ACCEPTED_CULTURE = 99999
-NDefines.NCountry.PS_REMOVE_ACCEPTED_CULTURE = 100
+NDefines.NCountry.CULTURE_MIN_DEVELOPMENT_TO_PROMOTE = 30		-- Minimum development required to promote the culture.
+NDefines.NCountry.PS_SET_PRIMARY_CULTURE = 400
+NDefines.NCountry.PS_ADD_ACCEPTED_CULTURE = 200
+NDefines.NCountry.PS_REMOVE_ACCEPTED_CULTURE = 200
 NDefines.NCountry.PS_REMOVE_ACCEPTED_CULTURE_UNREST_DURATION = 25
 
 
